23247aaeec3af358dd740569d349366fbce68a81
Tom Flitterman <tfl@pers-ee.com>
2023-07-21 19:14:24 +0200

    Set up the local storage automatically when reaching the App component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <TheHeader />
+  <TheHeader v-if="route.name !== 'practiceWords'" />
   <RouterView />
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router';
+import { useRoute, RouterView } from 'vue-router';
+import { useLanguagesStore } from '@/stores/languages';
 import TheHeader from '@/components/layout/TheHeader.vue';
+
+const route = useRoute();
+const languageStore = useLanguagesStore();
+
+if (!localStorage.getItem('data')) {
+  localStorage.setItem('data', JSON.stringify({}));
+} else {
+  console.log('loaded App.vue');
+  languageStore.$patch({
+    data: JSON.parse(localStorage.getItem('data')),
+  });
+}
 </script>
