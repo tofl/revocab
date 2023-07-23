a29ec62f12d2cd652a3aab6d8532827380f470c5
Tom Flitterman <tfl@pers-ee.com>
2023-07-23 19:51:13 +0200

    Added Analytics id

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import VueGtagPlugin from 'vue-gtag';
 
 import App from './App.vue';
 import router from './router';
@@ -10,5 +11,10 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueGtagPlugin, {
+    config: { id: import.meta.env.VITE_ANALYTICS_ID },
+});
+
+console.log(import.meta.env.VITE_ANALYTICS_ID);
 
 app.mount('#app');
