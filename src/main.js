f349be72479cc81589f6a8ae592a38f1e78209f8
Tom Flitterman <tfl@pers-ee.com>
2023-07-23 21:59:31 +0200

    Use GTM script instead of plugin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { createGtm } from '@gtm-support/vue-gtm';
 
 import App from './App.vue';
 import router from './router';
@@ -11,9 +10,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(createGtm({
-    id: import.meta.env.VITE_ANALYTICS_ID,
-}));
 
 console.log(import.meta.env.VITE_ANALYTICS_ID);
 
