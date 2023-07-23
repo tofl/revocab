03d6ec229c350800f4d7cc45deb87663161e1a86
Tom Flitterman <tfl@pers-ee.com>
2023-07-23 17:12:15 +0200

    Added route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="bg-main-green min-h-screen text-main-dark">
-    <TheHeader v-if="route.name !== 'practiceWords'" />
-    <RouterView />
+    <transition
+      leave-active-class="transition-opacity duration-200 ease-in-out"
+      enter-active-class="transition-opacity duration-200 ease-in-out"
+      enter-from-class="opacity-0"
+      leave-to-class="opacity-0"
+    >
+      <TheHeader v-if="route.name !== 'practiceWords'" />
+    </transition>
+
+    <RouterView v-slot="{ Component }">
+      <transition
+        mode="out-in"
+        leave-active-class="transition-opacity duration-200 ease-in-out"
+        enter-active-class="transition-opacity duration-200 ease-in-out"
+        enter-from-class="opacity-0"
+        leave-to-class="opacity-0"
+      >
+        <component :is="Component" />
+      </transition>
+    </RouterView>
   </div>
 </template>
 
