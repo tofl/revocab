48f8d175022d353161b69421623ca8224ba6cce4
Tom Flitterman <tfl@pers-ee.com>
2023-07-25 19:19:18 +0200

    Removed the header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="bg-main-green min-h-screen text-main-dark">
-    <transition
-      leave-active-class="transition-opacity duration-200 ease-in-out"
-      enter-active-class="transition-opacity duration-200 ease-in-out"
-      enter-from-class="opacity-0"
-      leave-to-class="opacity-0"
-    >
-      <TheHeader v-if="route.name !== 'practiceWords'" />
-    </transition>
+<!--    <TheHeader v-if="route.name !== 'practiceWords'" />-->
 
     <RouterView v-slot="{ Component }">
       <transition
@@ -24,11 +17,9 @@
 </template>
 
 <script setup>
-import { useRoute, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 import { useLanguagesStore } from '@/stores/languages';
-import TheHeader from '@/components/layout/TheHeader.vue';
 
-const route = useRoute();
 const languageStore = useLanguagesStore();
 
 if (!localStorage.getItem('data')) {
